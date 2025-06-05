827ebd30eea5aeeaf19458c9242e12c09a2fe9a6
calebhorst <76916817+calebhorst@users.noreply.github.com>
2025-06-05 03:30:35 +0000

    adding section for gsheets for community cloud deploy

--- pages/1_Bet_Logger.py
+++ pages/1_Bet_Logger.py
@@ -27,21 +27,9 @@ df = conn.read()
 
 sheet_name = "Gamblers Anonymous Streamlit"
 tab_name = "Master"
-credentials = {
-  "type": st.secrets["type"],
-  "project_id": st.secrets["project_id"],
-  "private_key_id": st.secrets["private_key_id"],
-  "private_key": st.secrets["private_key"],
-  "client_email": st.secrets["client_email"],
-  "client_id": st.secrets["client_id"],
-  "auth_uri": st.secrets["auth_uri"],
-  "token_uri": st.secrets["token_uri"],
-  "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
-  "client_x509_cert_url": st.secrets["client_x509_cert_url"],
-  "universe_domain": st.secrets["universe_domain"]
-}
 
-gc = gspread.service_account_from_dict(credentials)
+gc = gspread.service_account_from_dict(dict(st.secrets["gsheets"]))
+# gc = gspread.service_account(filename="secrets/google-credentials.json")
 google_sheet = gc.open(sheet_name)
 google_worksheet = google_sheet.worksheet(tab_name)
 
