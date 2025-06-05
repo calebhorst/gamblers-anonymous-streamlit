72d12262d49e05d8636d45e88052daaeafc0464a
calebhorst <76916817+calebhorst@users.noreply.github.com>
2025-06-05 02:43:27 +0000

    swap secrets from using git ignored file

--- pages/1_Bet_Logger.py
+++ pages/1_Bet_Logger.py
@@ -27,7 +27,21 @@ df = conn.read()
 
 sheet_name = "Gamblers Anonymous Streamlit"
 tab_name = "Master"
-gc = gspread.service_account(filename='secrets/google-credentials.json')
+credentials = {
+  "type": st.secrets["type"],
+  "project_id": st.secrets["project_id"],
+  "private_key_id": st.secrets["private_key_id"],
+  "private_key": st.secrets["private_key"],
+  "client_email": st.secrets["client_email"],
+  "client_id": st.secrets["client_id"],
+  "auth_uri": st.secrets["auth_uri"],
+  "token_uri": st.secrets["token_uri"],
+  "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
+  "client_x509_cert_url": st.secrets["client_x509_cert_url"],
+  "universe_domain": st.secrets["universe_domain"]
+}
+
+gc = gspread.service_account_from_dict(credentials)
 google_sheet = gc.open(sheet_name)
 google_worksheet = google_sheet.worksheet(tab_name)
 
